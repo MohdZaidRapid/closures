7ad17e20be8affcb551f3cd95abfdc397860b1de
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-25 16:58:07 +0530

    added

--- callbackfnc.js
+++ callbackfnc.js
@@ -1,13 +1,23 @@
 // What is Callback Function in Javascript
-setTimeout(() => {
-  console.log("timer");
-}, 5000);
+// setTimeout(() => {
+//   console.log("timer");
+// }, 5000);
 
-function x(y) {
-  console.log("x");
+// function x(y) {
+//   console.log("x");
+// }
+// x(function y() {
+//   console.log("y");
+// });
+
+// Javascript is single  threaded and sync languahe
+
+function attachEventListener() {
+  console.log("hello");
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("Button Clicked", ++count);
+  });
 }
-x(function y() {
-  console.log("y");
-});
 
-// Js is single  threaded and sync language
+attachEventListener();
