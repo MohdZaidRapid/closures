401a5d35d310366dad3a939d97b87c28bdc085a3
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-28 16:19:06 +0530

    added

--- season2/Promiese.js
+++ season2/Promiese.js
@@ -1,17 +1,28 @@
-// const cart = ["shoes", "pants", "kurta"];
+const cart = ["shoes", "pants", "kurta"];
 
-// createOrder(cart, function (orderId) {
-//   proceedToPayment(orderId);
-// });
+createOrder(cart, function (orderId) {
+  proceedToPayment(orderId, function (paymentInfo) {
+    showOrderSummary(paymentInfo, function () {
+      updateWalletBalance();
+    });
+  });
+});
 
-// const createOrder(cart)
+const promise = createOrder(cart);
 
-// promise.then(function(orderId){
-//     proceedToPayment(orderId)
-// })
+promise.then(function (orderId) {
+  proceedToPayment(orderId);
+});
 
-const GITHUB_USERNAME = "https://api.github.com/users/mohdzaid123";
+// const GITHUB_USERNAME = "https://api.github.com/users/mohdzaid123";
 
-const user = fetch(GITHUB_USERNAME);
+// const user = fetch(GITHUB_USERNAME);
 
-console.log(user);
+// console.log(user);
+
+// what is Promise chaining
+
+createOrder(cart)
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo) => showOrderSummary(paymentInfo))
+  .then((orderId) => updateWalletBalance(orderId));
