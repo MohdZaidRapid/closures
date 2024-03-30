e55b9ea44c43fc88808c0ebf0515e0053a55e501
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-30 16:27:37 +0530

    added

--- season2/PromiseType.js
+++ season2/PromiseType.js
@@ -1,5 +1,34 @@
 // Promise.all()
 
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("P1 success");
+//   }, 3000);
+// });
+// const p2 = new Promise((resolve, reject) => {
+//   //   setTimeout(() => {
+//   //     resolve("P2 success");
+//   //   }, 1000);
+//   setTimeout(() => {
+//     resolve("P2 success");
+//   }, 1000);
+// });
+// const p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("P3 success");
+//   }, 2000);
+// });
+
+// Promise.all([p1, p2, p3])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Promise.allSettled
+
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("P1 success");
@@ -15,11 +44,11 @@ const p2 = new Promise((resolve, reject) => {
 });
 const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P3 success");
+    reject("P3 fail");
   }, 2000);
 });
 
-Promise.all([p1, p2, p3])
+Promise.allSettled([p1, p2, p3])
   .then((res) => {
     console.log(res);
   })
